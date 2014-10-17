daf1121e7c1ab5c1f469f12eb15a4a147f2f0f52
Felix Shaw <felix.shaw@tgac.ac.uk>
2014-10-17 11:41:05 +0100

    updated some models comments, commiting before code reshuffle

--- polls/models.py
+++ polls/models.py
@@ -4,23 +4,28 @@ from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
+    #model entity fields
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
+
+    #model callable methods
     def __str__(self):
         return self.question_text
-        
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    #admin view methods
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published Recently?'
-    
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.choice_text
     
